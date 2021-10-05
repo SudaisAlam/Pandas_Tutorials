d251d288cede6c8c63a5e2350afa2f6e349f3cae
SudaisAlam <alamsudais72@gmail.com>
2021-10-06 04:59:31 +0500

    Accessing indexes using pandas

--- indexing,selecting.py
+++ indexing,selecting.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-Data = pd.read_csv('/Users/sudaisalam/Downloads/titanic/train.csv')
+Data = pd.read_csv('train.csv')
 
 # The Native way
 #it actually offers quite simple ways

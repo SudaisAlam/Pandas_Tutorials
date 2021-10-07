c2e5235810e46022968bde793550a26381bbfe37
SudaisAlam <alamsudais72@gmail.com>
2021-10-07 21:55:41 +0500

    Using the groupny function in pandas

--- Groupby.py
+++ Groupby.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-Data = pd.read_csv('/Users/sudaisalam/Downloads/titanic/train.csv')
+Data = pd.read_csv('train.csv')
 
 print(Data.groupby("Age").Age.count())
 print(Data.groupby("Fare").Fare.min())

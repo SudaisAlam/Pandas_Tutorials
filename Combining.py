b2ca93859317eea0d003edc8bf4cc61e8478bd64
SudaisAlam <alamsudais72@gmail.com>
2021-10-07 21:54:20 +0500

    Joining DataFrames in pandas

--- Combining.py
+++ Combining.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-Data = pd.read_csv('/Users/sudaisalam/Downloads/titanic/train.csv')
+Data = pd.read_csv('train.csv')
 
 print(pd.concat([Data.Name, Data.Survived]))
 print(Data[['Name', 'Age']].join(Data.Survived, lsuffix='_self', rsuffix='_tojoin'))# better!
